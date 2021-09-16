1aa4bd9875fe5f8a679770e87570a3ba96ce0673
AndreevDaniil2002 <90094074+AndreevDaniil2002@users.noreply.github.com>
2021-09-16 18:07:14 +0300

    Андреев Даниил ПИ20-1

--- client.py
+++ client.py
@@ -3,14 +3,16 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock.connect(('127.0.0.1', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+msg = input()
+while len(msg) != 0:
+    sock.send(msg.encode())
 
-data = sock.recv(1024)
+    data = sock.recv(1024)
+    if msg == 'exit':
+        sock.close()
+        break
 
-sock.close()
-
-print(data.decode())
+    print(data.decode())
+    msg = input()
