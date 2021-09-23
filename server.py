1021ff6f18ee5f2db3f7c076542c7d8d44d960c5
MikeMarshall36 <90028680+MikeMarshall36@users.noreply.github.com>
2021-09-23 17:35:01 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,17 +1,20 @@
 import socket
-from time import sleep
 
 sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('127.0.0.1', 9091))
+sock.bind(('', 9091))
+sock.listen(0)
+conn, addr = sock.accept()
+print(addr)
 
-msg = input()
-while len(msg) != 0:
-    sock.send(msg.encode())
-    data = sock.recv(1024)
-    exit_check = msg.lower()
-    if exit_check == 'exit':
-        sock.close()
-        break
-    print(data.decode())
-    msg = input()
+msg = ''
+
+while True:
+	data = conn.recv(1024)
+	if not data:
+		break
+	msg += data.decode()
+	conn.send(data)
+
+print(msg)
+
+conn.close()
