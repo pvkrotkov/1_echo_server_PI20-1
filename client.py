d532bcbcf39ad0d19b6c07a65b044988784096ef
DenisMeln <90024383+DenisMeln@users.noreply.github.com>
2021-10-21 17:59:12 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,14 +1,17 @@
 import socket
 from time import sleep
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
-msg = input()
-while len(msg) != 0:
-  sock.send(msg.encode())
-  data = sock.recv(1024)
-  if_exit = msg.lower()
-  if if_exit == "exit": 
-    sock.close()
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = input('Введите IP-адрес входа:')
+port = 9090
+address = (host, port)
+sock.connect((host, port))
+print('Введите exit, чтобы выйти.')
+while True:
+  data_out = input('Введите сообщение:')
+  if data_out == 'exit':
     break
-  print(data.decode())
+  while len(data_out) != 0:
+    sock.send(data_out.encode())
+  data_in = sock.recv(1024)
+  print(data_in.decode())
+ sock.close()
