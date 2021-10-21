0edf6b3b65753f81681fe039f8f4ca8560fd2b3f
DenisMeln <90024383+DenisMeln@users.noreply.github.com>
2021-10-21 18:06:51 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,26 +1,19 @@
 import socket
-import threading
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+port = 9090
 
+sock.bind((host, port))
+sock.listen(4)
+conn, address = sock.accept()
+print(address)
+data_out = ''
+while True:
+	data_in = conn.recv(1024)
+	if not data_in:
+		break
+	data_out += data_in.decode()
+	conn.send(data_in)
 
-sock.bind(('', 9090))
-sock.listen(1)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
-
-def proc():
-	while True:
-		data = conn.recv(1024)
-		if not data:
-			break
-		msg += data.decode()
-		conn.send(data)
-
-print(msg)
-
-p1 = threading.Thread(target = proc, name = "t1")
-p1.start()
+print(data_out)
 conn.close()
