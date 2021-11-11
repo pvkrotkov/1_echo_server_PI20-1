462ab2a5f243a0cef8eeba778b59a107885565ff
DenisMeln <90024383+DenisMeln@users.noreply.github.com>
2021-11-11 15:43:16 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,23 @@
-import socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = socket.gethostname()
+from socket import *
+udp_sock = socket(AF_INET, SOCK_STREAM)
+host = gethostname()
 port = 9090
-sock.bind((host, port))
-sock.listen(4)
-conn, address = sock.accept()
+udp_sock.bind((host, port))
+udp_sock.listen(4)
+conn, address = udp_sock.accept()
 print(address)
 data_out = ''
+client_list = []
 while True:
 	data_in = conn.recv(1024)
+	if not (address in client_list):
+		client_list.append(address)
 	if not data_in:
 		break
 	data_out += data_in.decode()
 	conn.send(data_in)
-print(data_out)
-conn.close()
+	for i in client_list:
+		if i != address:
+			udp_socket.sendto(b'hello', i)
+	print(data_out)
+udp_socket.close()
