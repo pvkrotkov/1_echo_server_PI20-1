ca138df8b6caf49eae0c988ce36fba39de247d5f
shevlyakovava <90023991+shevlyakovava@users.noreply.github.com>
2021-11-11 17:30:43 +0300

    Update server.py
    Шевлякова ПИ20-1

--- server.py
+++ server.py
@@ -1,20 +1,30 @@
+#!/usr/bin/env python3
+ 
 import socket
-
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
-
-msg = ''
-
+from datetime import datetime
+ 
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+ 
+print('Сервер включен')
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
+    current_time = datetime.now().time()
+ 
+ 
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print('Прослушивание порта...')
+        conn, addr = s.accept()
+        with conn:
+            print(f'Connected by {addr} in %d:%d:%d ' % (current_time.hour, current_time.minute, current_time.second))
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+                check = data.decode("utf-8")
+                print(f'Отправка данных клиенту - {data.decode("utf-8")}')
+            if check == 'exit':
+                print('Соединение разорвано')
+                conn.close()
