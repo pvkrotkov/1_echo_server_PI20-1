e1ce4d8255184767c9c3f79048ed72bd7aae20e8
shevlyakovava <90023991+shevlyakovava@users.noreply.github.com>
2021-11-11 17:31:34 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
- 
+
 import socket
 from datetime import datetime
- 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
- 
+
+HOST = '127.0.0.1'
+PORT = 65432
+
 print('Сервер включен')
 while True:
     current_time = datetime.now().time()
- 
- 
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
         print('Прослушивание порта...')
+
         conn, addr = s.accept()
+
+
+        print(f'Connected by {addr} in %d:%d:%d ' % (current_time.hour, current_time.minute, current_time.second))
         with conn:
-            print(f'Connected by {addr} in %d:%d:%d ' % (current_time.hour, current_time.minute, current_time.second))
             while True:
                 data = conn.recv(1024)
                 if not data:
