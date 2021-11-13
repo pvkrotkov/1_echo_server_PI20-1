5416303c9e388a51a88d0fdebabcdfd7e8a9d0bd
shevlyakovava <90023991+shevlyakovava@users.noreply.github.com>
2021-11-14 01:37:10 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,24 @@
+#!/usr/bin/env python3
+
 import socket
-from time import sleep
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
-
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
-sock.close()
-
-print(data.decode())
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+while True:
+    command = input()
+    if command != 'exit':
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            s.sendall(command.encode())
+            data = s.recv(1024)
+            print('Received', data.decode('UTF-8'))
+    if command == 'exit':
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            s.sendall(b'exit')
+            data = s.recv(1024)
+            print('Received', data.decode('UTF-8'))
+            print('Соединение разорвано')
+        break
+    else:
+        continue
