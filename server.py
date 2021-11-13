c502327cc748cffd1f31df1f6b3f224be4868f98
shevlyakovava <90023991+shevlyakovava@users.noreply.github.com>
2021-11-14 01:37:23 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,32 @@
+#!/usr/bin/env python3
+
 import socket
+from datetime import datetime
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+HOST = '127.0.0.1'
+PORT = 65432
 
-msg = ''
-
+print('Сервер включен')
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    current_time = datetime.now().time()
 
-print(msg)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print('Прослушивание порта...')
 
-conn.close()
+        conn, addr = s.accept()
+
+
+        print(f'Connected by {addr} in %d:%d:%d ' % (current_time.hour, current_time.minute, current_time.second))
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+                check = data.decode("utf-8")
+                print(f'Отправка данных клиенту - {data.decode("utf-8")}')
+            if check == 'exit':
+                print('Соединение разорвано')
+                conn.close()
